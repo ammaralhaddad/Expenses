5bfc826e745dde30ffe77c859f62dad3ab39b670
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-21 17:30:38 -0500

    add props toi conecct the expenseItem to app.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,16 +2,16 @@ import React from "react";
 
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-  const expenseTitle = "Live insurance";
-  const expenseDate = new Date(2021, 12, 12);
-  const expencePrice = 299.99;
+export default function ExpenseItem(props) {
+  // const expenseTitle = "Live insurance";
+  // const expenseDate = new Date(2021, 12, 12);
+  // const expencePrice = 299.99;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expencePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
