fc1619aa71a7eddb65e72275c9a148f6f54a0fd6
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-21 16:47:02 -0500

    create the  style folder for the expense item aand add them to the expenseitem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,15 @@
-export default function ExpenseItem() {}
+import React from "react";
+
+import "./ExpenseItem.css";
+
+export default function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28</div>
+      <div>
+        <h2 className="expense-item__description">Car insurnce</h2>
+        <div className="expense-item__price">299.99</div>
+      </div>
+    </div>
+  );
+}
