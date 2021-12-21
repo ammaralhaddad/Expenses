c4276865a43a68e84679e603c8b1ef36c8e1604c
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-21 17:09:55 -0500

    refacter the express item with dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,12 +3,15 @@ import React from "react";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const expenseTitle = "Live insurance";
+  const expenseDate = new Date(2021, 12, 12);
+  const expencePrice = 299.99;
   return (
     <div className="expense-item">
-      <div>March 28</div>
-      <div>
-        <h2 className="expense-item__description">Car insurnce</h2>
-        <div className="expense-item__price">299.99</div>
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expencePrice}</div>
       </div>
     </div>
   );
