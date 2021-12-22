398d1d1195a1dcc98ba8df998d0737ca02973470
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-21 21:01:17 -0500

    clean the components folder

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       {/* // we have to pass props to date  to access the date  */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
